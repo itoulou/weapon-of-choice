5b03971ce65c6ff0154e7f826908ab5cee2a12a1
itoulou <touloumbadjiani@gmail.com>
2023-01-24 15:07:54 +0000

    revert chnages

--- testdatabase/migrations/20221219142001-create-track.js
+++ testdatabase/migrations/20221219142001-create-track.js
@@ -17,14 +17,6 @@ module.exports = {
       },
       Playlist_playlistId: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
