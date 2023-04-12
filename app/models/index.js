bca1507aaab819d220cfd34822802b14c2a46434
itoulou <touloumbadjiani@gmail.com>
2023-04-12 12:50:51 +0100

    change db connection

--- app/models/index.js
+++ app/models/index.js
@@ -6,14 +6,16 @@ let sequelize;
 console.log("ENV=> ", config.util.getEnv('NODE_ENV'));
 console.log("DB_URI=> ", process.env.DB_URI)
 if (config.util.getEnv('NODE_ENV') !== 'staging' && config.util.getEnv('NODE_ENV') !== 'prod') {
-    sequelize = new Sequelize(`postgres://${config.db.username}:${config.db.password}@${config.db.host}:5432/${config.db.database}`, {dialect: 'postgres', logging: logging});
-} else {
-    sequelize = new Sequelize(process.env.DB_URI, {dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }, logging: logging});
+    
 }
+sequelize = new Sequelize(`postgres://${config.db.username}:${config.db.password}@${config.db.host}:5432/${config.db.database}`, {dialect: 'postgres', logging: logging});
+// else {
+//     sequelize = new Sequelize(process.env.DB_URI, {dialectOptions: {
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     }, logging: logging});
+// }
 
 sequelize.authenticate().then(() => {
     console.log(`Database connected to ${sequelize.config.database}`);
